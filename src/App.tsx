e0976b93be8eb29ea4807b77eb71e71e71f3b9ea
SPavanRaj <spavankumarp457@gmail.com>
2026-02-12 23:50:12 +0530

    fix blank screen working version
    Refactored App component to remove SafetyProvider and AppContent. Added a message indicating SurakshaAI is working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import React, { useState } from 'react';
-import { SafetyProvider } from './context/SafetyContext';
-import { Layout } from './components/Layout';
-import { Dashboard } from './pages/Dashboard';
-import { SmartRoute } from './pages/SmartRoute';
-import { Contacts } from './pages/Contacts';
-import { ActivityLogPage } from './pages/ActivityLog';
-import { Settings } from './pages/Settings';
-
-const AppContent = () => {
-  const [currentView, setCurrentView] = useState('dashboard');
-
-  const renderView = () => {
-    switch (currentView) {
-      case 'dashboard': return <Dashboard />;
-      case 'routes': return <SmartRoute />;
-      case 'contacts': return <Contacts />;
-      case 'activity': return <ActivityLogPage />;
-      case 'settings': return <Settings />;
-      default: return <Dashboard />;
-    }
-  };
-
+function App() {
   return (
-    <Layout currentView={currentView} onChangeView={setCurrentView}>
-      {renderView()}
-    </Layout>
+    <div style={{ padding: "40px", fontSize: "30px" }}>
+      🚀 SurakshaAI is Working!
+    </div>
   );
-};
-
-const App: React.FC = () => {
-  return (
-    <SafetyProvider>
-      <AppContent />
-    </SafetyProvider>
-  );
-};
+}
 
 export default App;
